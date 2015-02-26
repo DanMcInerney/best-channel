d44f325ecdc837cdf95a5f74c42f1976defca093
Dan McInerney <danhmcinerney@gmail.com>
2015-02-25 21:31:18 -0700

    finished

--- best-channel.py
+++ best-channel.py
@@ -27,7 +27,7 @@ C  = '\033[36m' # cyan
 GR = '\033[37m' # gray
 T  = '\033[93m' # tan
 
-channels = {}
+channels = {1:{}, 6:{}, 11:{}}
 
 def parse_args():
 	#Create the arguments
@@ -104,14 +104,15 @@ def get_iface(interfaces):
             return interface
 
 def start_mon_mode(interface):
-    print '['+G+'+'+W+'] Starting monitor mode off '+G+interface+W
+    print '['+G+'+'+W+'] Starting monitor mode on '+G+interface+W
     try:
-        os.system('ifconfig %s down' % interface)
-        os.system('iwconfig %s mode monitor' % interface)
-        os.system('ifconfig %s up' % interface)
+        os.system('/sbin/ifconfig %s down' % interface)
+        os.system('/sbin/iwconfig %s mode monitor' % interface)
+        os.system('/sbin/ifconfig %s up' % interface)
         return interface
     except Exception:
         sys.exit('['+R+'-'+W+'] Could not start monitor mode')
+        raise ####################
 
 def remove_mon_iface(mon_iface):
     os.system('/sbin/ifconfig %s down' % mon_iface)
@@ -132,7 +133,6 @@ def mon_mac(mon_iface):
 # End of interface info and manipulation
 ########################################
 
-
 def cb(pkt):
     '''
     Look for dot11 packets that aren't to or from broadcast address,
@@ -149,22 +149,45 @@ def cb(pkt):
                 APs(pkt)
 
 def APs(pkt):
-    ssid       = pkt[Dot11Elt].info
-    bssid      = pkt[Dot11].addr3
+    ssid = pkt[Dot11Elt].info
+    bssid = pkt[Dot11].addr3
     try:
-        ap_channel = str(ord(pkt[Dot11Elt:3].info))
         chans = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11']
+        # http://stackoverflow.com/questions/10818661/scapy-retrieving-rssi-from-wifi-packets
+        sig_str = -(256-ord(pkt.notdecoded[-4:-3]))
+        # airoscapy
+        ap_channel = str(ord(pkt[Dot11Elt:3].info))
         if ap_channel in chans:
-            print ap_channel
-    except Exception as e:
+            if ap_channel in ['1','2','3']:
+                # Set to {MAC address:avg power signal}
+                if bssid in channels[1]:
+                    channels[1][bssid].append(sig_str)
+                else:
+                    channels[1][bssid] = [sig_str]
+            elif ap_channel in ['4','5','6','7','8']:
+                # Set to {MAC address:avg power signal}
+                if bssid in channels[6]:
+                    channels[6][bssid].append(sig_str)
+                else:
+                    channels[6][bssid] = [sig_str]
+            elif ap_channel in ['9', '10', '11']:
+                # Set to {MAC address:avg power signal}
+                if bssid in channels[11]:
+                    channels[11][bssid].append(sig_str)
+                else:
+                    channels[11][bssid] = [sig_str]
+    except Exception:
         raise
 
-def output(err, monchannel):
+def output(err, num_aps, chan_interference, best_channel):
     os.system('clear')
     if err:
         print err
     else:
-        print '['+G+'+'+W+'] '+mon_iface+' channel: '+G+monchannel+W+'\n'
+        print '[Channels '+G+'1-3'+W+']  Number of APs: '+T+'%d' % num_aps[1], W+' |  Interference level: '+R+'%d' % chan_interference[1], W
+        print '[Channels '+G+'4-8'+W+']  Number of APs: '+T+'%d' % num_aps[6], W+' |  Interference level: '+R+'%d' % chan_interference[6], W
+        print '[Channels '+G+'9-11'+W+'] Number of APs: '+T+'%d' % num_aps[11], W+' |  Interference level: '+R+'%d' % chan_interference[11], W
+        print '['+G+'+'+W+'] Recommended channel: '+G+'%s' % best_channel, W
 
 def channel_hop(mon_iface):
     '''
@@ -192,16 +215,74 @@ def channel_hop(mon_iface):
         for line in proc.communicate()[1].split('\n'):
             if len(line) > 2: # iw dev shouldnt display output unless there's an error
                 err = '['+R+'-'+W+'] Channel hopping failed: '+R+line+W
+
+        # avg_pwr_per_ap = {1:{bssid:ap_pwr}, 6:[...], 11:[...]}
+        avg_pwr_per_ap = get_ap_pwr()
+        num_aps = get_num_aps(avg_pwr_per_ap)
+        chan_interference = get_chan_interference(avg_pwr_per_ap)
+        best_channel = get_best_channel(chan_interference)
+
+        output(err, num_aps, chan_interference, best_channel)
         time.sleep(.5)
 
-        output(err, monchannel)
+def get_best_channel(chan_interference):
+    '''
+    Get the channel with the least interference
+    '''
+    if chan_interference[1] == chan_interference[6] == chan_interference[11]:
+        return 'All the same'
+
+    least = min([chan_interference[1], chan_interference[6], chan_interference[11]])
+    if least == chan_interference[1]:
+        return '1'
+    elif least == chan_interference[6]:
+        return '6'
+    elif least == chan_interference[11]:
+        return '11'
+
+def get_chan_interference(avg_pwr_per_ap):
+    '''
+    Add together all the BSSID's avg pwr levels
+    '''
+    interference_val = {1:0, 6:0, 11:0}
+    total_power = {1:[], 6:[], 11:[]}
+    for chan in [1, 6, 11]:
+        for bssid in avg_pwr_per_ap[chan]:
+            total_power[chan].append(avg_pwr_per_ap[chan][bssid])
+        interference_val[chan] = -sum(total_power[chan])
+
+    return interference_val
+
+def get_ap_pwr():
+    '''
+    Returns a dict of nonoverlapping channels which contains
+    a dict of each BSSID's average power
+    '''
+    avg_pwr_per_ap = {1:{}, 6:{}, 11:{}}
+    for chan in [1, 6, 11]:
+        # channels[chan] = {'bssid':[avgpwr, avgpwr]}
+        for bssid in channels[chan]:
+            bssid_avg_pwr = float(sum(channels[chan][bssid]))/len(channels[chan][bssid]) if len(channels[chan][bssid]) > 0 else 0
+            avg_pwr_per_ap[chan][bssid] = bssid_avg_pwr
+
+    return avg_pwr_per_ap
+
+def get_num_aps(avg_pwr_per_ap):
+
+    num_aps = {1:0, 6:0, 11:0}
+    for chan in [1, 6, 11]:
+        num_aps[chan] = len(avg_pwr_per_ap[chan])
+    return num_aps
 
 def stop(signal, frame):
     if monitor_on:
-        sys.exit('\n['+R+'!'+W+'] Closing')
+        sys.exit(
+        '\n['+R+'!'+W+'] Closing... You will probably have to reconnect to your \
+wireless network if you were on one prior to running this script')
     else:
         remove_mon_iface(mon_iface)
-        sys.exit('\n['+R+'!'+W+'] Closing')
+        sys.exit('\n['+R+'!'+W+'] Closing... You will probably have to reconnect to your \
+wireless network if you were on one prior to running this script')
 
 if __name__ == "__main__":
     if os.geteuid():
@@ -223,5 +304,5 @@ if __name__ == "__main__":
        sniff(iface=mon_iface, store=0, prn=cb)
     except Exception as msg:
         remove_mon_iface(mon_iface)
-        print '\n['+R+'!'+W+'] Sniffing failed, closing'
+        print '\n['+R+'!'+W+'] Sniffing failed: %s' % str(msg)
         sys.exit(0)
